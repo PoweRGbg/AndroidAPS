4b42011bcbaff52d2b8d475d83656c6ccde6b9b1
Roumen Georgiev <roumen_georgiev@yahoo.com>
2020-03-05 10:43:37 +0200

    ActionStartTimer

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionStartTimer.java
@@ -0,0 +1,96 @@
+package info.nightscout.androidaps.plugins.general.automation.actions;
+
+import android.widget.LinearLayout;
+
+import com.google.common.base.Optional;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import info.nightscout.androidaps.MainApp;
+import info.nightscout.androidaps.R;
+import info.nightscout.androidaps.data.PumpEnactResult;
+import info.nightscout.androidaps.plugins.general.automation.elements.InputDuration;
+import info.nightscout.androidaps.plugins.general.automation.elements.InputString;
+import info.nightscout.androidaps.plugins.general.automation.elements.LabelWithElement;
+import info.nightscout.androidaps.plugins.general.automation.elements.LayoutBuilder;
+import info.nightscout.androidaps.queue.Callback;
+import info.nightscout.androidaps.utils.JsonHelper;
+
+public class ActionStartTimer extends Action {
+    private static final Logger log = LoggerFactory.getLogger(ActionNotification.class);
+
+    public InputString timerName = new InputString();
+    InputDuration timerDuration = new InputDuration(0, InputDuration.TimeUnit.MINUTES);
+    Long timerStartedAt = 0l;
+
+
+    @Override
+    public int friendlyName() {
+        return R.string.notification;
+    }
+
+    @Override
+    public String shortDescription() {
+        return MainApp.gs(R.string.clock_message, timerName.getValue());
+    }
+
+    @Override
+    public void doAction(Callback callback) {
+        timerStartedAt = System.currentTimeMillis();
+        if (callback != null)
+            callback.result(new PumpEnactResult().success(true).comment(R.string.ok)).run();
+
+    }
+
+    @Override
+    public Optional<Integer> icon() {
+        return Optional.of(R.drawable.icon_actions_startextbolus);
+    }
+
+    @Override
+    public String toJSON() {
+        JSONObject o = new JSONObject();
+        JSONObject data = new JSONObject();
+        try {
+            data.put("name", timerName.getValue());
+            data.put("duration", timerDuration.getValue());
+            data.put("startedAt", timerStartedAt);
+            o.put("type", this.getClass().getName());
+            o.put("data", data);
+        } catch (JSONException e) {
+            log.error("Unhandled exception", e);
+        }
+        return o.toString();
+    }
+
+    @Override
+    public Action fromJSON(String data) {
+        try {
+            JSONObject o = new JSONObject(data);
+            timerName.setValue(JsonHelper.safeGetString(o, "name"));
+            timerDuration.setMinutes(JsonHelper.safeGetInt(o, "duration"));
+            timerStartedAt = JsonHelper.safeGetLong(o, "startedAt");
+        } catch (JSONException e) {
+            log.error("Unhandled exception", e);
+        }
+        return this;
+    }
+
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    @Override
+    public void generateDialog(LinearLayout root) {
+
+        new LayoutBuilder()
+                .add(new LabelWithElement(MainApp.gs(R.string.message_short), "", timerName))
+                .add(new LabelWithElement(MainApp.gs(R.string.careportal_newnstreatment_duration_min_label), "", timerDuration))
+                .build(root);
+    }
+
+}
