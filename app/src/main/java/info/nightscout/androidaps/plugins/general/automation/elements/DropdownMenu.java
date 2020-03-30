0fe05e89a4559dc327bf6550f49f08dcf0c9134f
Roumen Georgiev <roumen_georgiev@yahoo.com>
2020-03-30 15:09:18 +0300

    Update DropdownMenu.java

--- app/src/main/java/info/nightscout/androidaps/plugins/general/automation/elements/DropdownMenu.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/automation/elements/DropdownMenu.java
@@ -10,6 +10,7 @@ import android.widget.Spinner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 
 import info.nightscout.androidaps.MainApp;
 import info.nightscout.androidaps.R;
